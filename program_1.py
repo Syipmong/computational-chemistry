308b24c516e5d360e77cc73e0452cef296ada585
syipmong <syipmong5@gmail.com>
2023-12-28 18:27:53 +0100

    assignment for ola

---
+++ program_1.py
@@ -0,0 +1,26 @@
+#write your name and age variables and also print it to the screen
+
+fname = "Ola"
+lname = "Adel"
+age = 80
+
+
+print("My name is "+ fname+ " " +lname)
+
+"""Instructions:
+
+Variable Declaration:
+
+Declare five variables: name, age, city, hobby, and favorite_color.
+Assign your name to the name variable, your age to the age variable, the name of your city to the city variable, your favorite hobby to the hobby variable, and your favorite color to the favorite_color variable.
+Print to Console - Basic Information:
+
+Use the print function to display the following basic information on the console:
+Your name
+Your age
+Your city
+Print to Console - Additional Information:
+
+Use the print function to display the following additional information on the console:
+Your favorite hobby
+Your favorite color"""
